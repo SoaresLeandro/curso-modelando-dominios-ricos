9781de64a59b62754bca0e0214c96935ca2d749b
Leandro <lss.91@live.com>
2024-11-28 16:48:18 -0400

    Criando Commands para CQRS

---
+++ PaymentContext/PaymentContext.Domain/Commands/CreatePayPalSubscriptionCommand.cs
@@ -0,0 +1,29 @@
+using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.Commands;
+
+namespace PaymentContext.Domain.Commands;
+
+public class CreatePayPalSubscriptionCommand : ICommand
+{
+    public string FisrtName { get; private set;}
+    public string LastName { get; private set; }
+    public string Document { get; private set; }
+    public string Email { get; private set; }
+    public string TransactionCode { get; private set; }
+    public string PaymentNumber { get; private set; }
+    public DateTime PaidDate { get; private set; }
+    public DateTime ExpireDate { get; private set; }
+    public decimal Total { get; private set; }
+    public decimal TotalPaid { get; private set; }
+    public string Payer { get; private set; }
+    public string PayerEmail { get; private set; }
+    public string PayerDocument { get; private set; }
+    public EDocumentType PayerDocumentType { get; private set; }
+    public string Street { get; private set; }
+    public string Number { get; set; }
+    public string Neighborhood { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string ZipCode { get; set; }
+}
